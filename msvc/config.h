f1edf1797803bac4c7f2073cbeb163b129868c78
vyd00767-spec <vyd00767@gmail.com>
2026-02-25 17:20:37 +0900

    Update config.h

--- msvc/config.h
+++ msvc/config.h
@@ -1,4 +1,4 @@
-/* config.h.  Manual config for MSVC.  */
+/* config.h.  Manual config for MSVC. */
 
 #ifndef _MSC_VER
 #warn "msvc/config.h shouldn't be included for your development environment."
